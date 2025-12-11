714881fc487966b3a078b11eae9fa4448feea752
unknown <iaraamancio1986@gmail.com>
2025-12-11 11:21:51 -0300

    adicionando animacao com motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // App.js
 import React from 'react';
 import Foto_iara from './assets/foto_iara.jpg';
+
+import { motion } from 'framer-motion';
+
 import BootStrap from './assets/icones-tecnologias/bootstrap.png';
 import Html from './assets/icones-tecnologias/html.png';
 import CSS from './assets/icones-tecnologias/css.png';
@@ -26,6 +29,12 @@ function App() {
   return (
     <div className="bg-black text-white min-h-screen">
       {/* Seção Inicial */}
+      <motion.div
+        initial={{ x: "-100%" }}
+        whileInView={{ x: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: false }}
+    >
       <section id="inicio" className="flex items-center justify-center w-full px-4 min-h-screen">
         <div className="grid grid-cols-1 md:grid-cols-2 md:justify-items-end justify-items-center gap-8 w-full max-w-4xl">
           <div className="flex flex-col justify-center items-center">
@@ -45,9 +54,15 @@ function App() {
           </div>
         </div>
       </section>
+    </motion.div>
 
+    <motion.div
+      initial={{ x: "-100%" }}
+        whileInView={{ x: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: false }}>
       {/* Seção Sobre Mim */}
-      <section id="sobre" className="flex justify-center items-center px-4 mt-8">
+      <section id="sobre" className="mt-8 flex justify-center items-center px-4 animated-element">
         <div className="text-center max-w-4xl">
           <h2 className="text-3xl md:text-5xl font-bold mb-6">Sobre Mim</h2>
           <p className="text-lg md:text-xl leading-relaxed">
@@ -56,120 +71,143 @@ function App() {
             Sou apaixonada por desenvolvimento web e comprometida com performance, acessibilidade e a entrega de interfaces de alta qualidade.
           </p>
         </div>
-      </section>
+      </section>      
+    </motion.div>
+
 
       {/* Seção Habilidades */}
-      <section id="habilidades" className="px-4 py-16 mt-8">
-        <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Habilidades</h2>
-        <div className="grid grid-cols-2 md:grid-cols-6 gap-8 max-w-6xl mx-auto">
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={Html} alt="Html" className="mx-auto mb-4 rounded h-32 w-32" />
-            <p className="text-xl font-semibold">HTML</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={CSS} alt="Css" className="mx-auto mb-4 rounded h-32 w-32" />
-            <p className="text-xl font-semibold">CSS</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={JavaScript} alt="JavaScript" className="mx-auto mb-4 rounded h-32 w-32" />
-            <p className="text-xl font-semibold">JavaScript</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={TypeScript} alt="TypeScript" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">TypeScript</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={Next} alt="Next.js" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">Next.js</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={ReactIcone} alt="React" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">React</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={Tailwind} alt="Tailwind" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">Tailwind CSS</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={BootStrap} alt="Bootstrap" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">BootStrap</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={Redux} alt="Redux" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">Redux</p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={Node} alt="Node.js" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
-            <p className="text-xl font-semibold">Node.js</p>
-          </div>
-        </div>
-      </section>
+      <motion.div  
+        initial={{ x: "-100%" }}
+        whileInView={{ x: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: false }}>
+          <section id="habilidades" className="px-4 py-16 mt-8 animated-element">
+            <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Habilidades</h2>
+            <div className="grid grid-cols-2 md:grid-cols-6 gap-8 max-w-6xl mx-auto">
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={Html} alt="Html" className="mx-auto mb-4 rounded h-32 w-32" />
+                <p className="text-xl font-semibold">HTML</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={CSS} alt="Css" className="mx-auto mb-4 rounded h-32 w-32" />
+                <p className="text-xl font-semibold">CSS</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={JavaScript} alt="JavaScript" className="mx-auto mb-4 rounded h-32 w-32" />
+                <p className="text-xl font-semibold">JavaScript</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={TypeScript} alt="TypeScript" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">TypeScript</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={Next} alt="Next.js" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">Next.js</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={ReactIcone} alt="React" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">React</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={Tailwind} alt="Tailwind" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">Tailwind CSS</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={BootStrap} alt="Bootstrap" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">BootStrap</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={Redux} alt="Redux" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">Redux</p>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={Node} alt="Node.js" className="mx-auto mb-4 rounded h-32 w-32 object-contain" />
+                <p className="text-xl font-semibold">Node.js</p>
+              </div>
+            </div>
+          </section>        
+      </motion.div>
+     
 
       {/* Seção Meus Projetos */}
-      <section id="projetos" className="px-4 py-16">
-        <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Meus Projetos</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={FotoProjeto1} alt="Projeto 1" className="w-full h-1/2 mb-4 rounded object-cover" />
-            <h3 className="text-xl font-semibold mb-2">Planejador de Atividades</h3>
-            <p className="mb-4 text-gray-400">Um site para gerenciar tarefas diárias, desenvolvido com JavaScript.</p>
-            <a href='https://iaraamancio.github.io/ProjetoRocketSeat/'>            
-              <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
-            </a>          
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={FotoProjeto2} alt="Projeto 2" className="w-full h-1/2 mb-4 rounded object-cover" />
-            <h3 className="text-xl font-semibold mb-2">Library Web Service</h3>
-            <p className="mb-4 text-gray-400">A plataforma mostra artigos de uma API e resultados de busca, usando JavaScript e React.js</p>
-            <a href='https://iaraamancio.github.io/LibraryWebService/'>
-              <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
-            </a>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
-            <img src={FotoProjeto3} alt="Projeto 3" className="w-full h-1/2 mb-4 rounded object-cover" />
-            <h3 className="text-xl font-semibold mb-2">Consulta de Pokemons</h3>
-            <p className="mb-4">Consumindo uma API com HTML, CSS e JavaScript.</p>
-            <a href='https://iaraamancio.github.io/Consumindo-Uma-API-de-Pokemon-com-Javascript/'>
-              <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
-            </a>
-          </div>
-        </div>
-      </section>
+      <motion.div
+        initial={{ x: "-100%" }}
+        whileInView={{ x: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: false }}>
+          <section id="projetos" className="px-4 py-16 animated-element">
+            <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Meus Projetos</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={FotoProjeto1} alt="Projeto 1" className="w-full h-1/2 mb-4 rounded object-cover" />
+                <h3 className="text-xl font-semibold mb-2">Planejador de Atividades</h3>
+                <p className="mb-4 text-gray-400">Um site para gerenciar tarefas diárias, desenvolvido com JavaScript.</p>
+                <a href='https://iaraamancio.github.io/ProjetoRocketSeat/'>            
+                  <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
+                </a>          
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={FotoProjeto2} alt="Projeto 2" className="w-full h-1/2 mb-4 rounded object-cover" />
+                <h3 className="text-xl font-semibold mb-2">Library Web Service</h3>
+                <p className="mb-4 text-gray-400">A plataforma mostra artigos de uma API e resultados de busca, usando JavaScript e React.js</p>
+                <a href='https://iaraamancio.github.io/LibraryWebService/'>
+                  <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
+                </a>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-lg text-center hover:bg-gray-700 transition">
+                <img src={FotoProjeto3} alt="Projeto 3" className="w-full h-1/2 mb-4 rounded object-cover" />
+                <h3 className="text-xl font-semibold mb-2">Consulta de Pokemons</h3>
+                <p className="mb-4">Consumindo uma API com HTML, CSS e JavaScript.</p>
+                <a href='https://iaraamancio.github.io/Consumindo-Uma-API-de-Pokemon-com-Javascript/'>
+                  <button className="bg-white text-black px-4 py-2 rounded font-semibold hover:bg-gray-200 transition">Ver Mais</button>
+                </a>
+              </div>
+            </div>
+          </section>
+
+      </motion.div>
 
       {/* Seção Meus Contatos */}
-      <section id="contatos" className="px-4 py-16">
-        <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Meus Contatos</h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-12 max-w-3xl mx-auto">
-          <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
-            <div className='bg-gray-800 p-4 mb-3 rounded-full'>
-               <FaWhatsapp size={50}/>
+      <motion.div
+        initial={{ x: "-100%" }}
+        whileInView={{ x: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: false }}>
+        <section id="contatos" className="px-4 py-16">
+          <h2 className="text-3xl md:text-5xl font-bold text-center mb-12">Meus Contatos</h2>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-12 max-w-3xl mx-auto">
+            <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
+              <div className='bg-gray-800 p-4 mb-3 rounded-full'>
+                <FaWhatsapp size={50}/>
+              </div>
+              <a href="https://wa.me/5588999966424" className="text-xl font-semibold hover:underline">WhatsApp</a>
+              <p className='text-gray-400'>88 99996-6424</p>
             </div>
-            <a href="https://wa.me/5588999966424" className="text-xl font-semibold hover:underline">WhatsApp</a>
-            <p className='text-gray-400'>88 99996-6424</p>
-          </div>
-          <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
-            <div className='bg-gray-800 p-4 mb-3 rounded-full'>
-              <FaGithub size={50}/>
+            <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
+              <div className='bg-gray-800 p-4 mb-3 rounded-full'>
+                <FaGithub size={50}/>
+              </div>
+              <a href="https://github.com/IaraAmancio" className="text-xl font-semibold hover:underline">GitHub</a>
+              <p className='text-gray-400'>@IaraAmancio</p>
             </div>
-            <a href="https://github.com/IaraAmancio" className="text-xl font-semibold hover:underline">GitHub</a>
-            <p className='text-gray-400'>@IaraAmancio</p>
-          </div>
-          <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
-            <div className='bg-gray-800 p-4 mb-3 rounded-full'>
-              <FaLinkedin size={50}/>
+            <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
+              <div className='bg-gray-800 p-4 mb-3 rounded-full'>
+                <FaLinkedin size={50}/>
+              </div>
+              <a href="https://www.linkedin.com/in/iara-amancio-48aa85231/" className="text-xl font-semibold hover:underline">LinkedIn</a>
+              <p className='text-gray-400'>@IaraAmancio</p>
             </div>
-            <a href="https://www.linkedin.com/in/iara-amancio-48aa85231/" className="text-xl font-semibold hover:underline">LinkedIn</a>
-            <p className='text-gray-400'>@IaraAmancio</p>
-          </div>
-          <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
-            <div className='bg-gray-800 p-4 mb-3 rounded-full'>
-              <MdEmail size={50}/>
+            <div className="flex flex-col items-center p-6 text-center hover:scale-110 transition">
+              <div className='bg-gray-800 p-4 mb-3 rounded-full'>
+                <MdEmail size={50}/>
+              </div>
+              <a href="mailto:iaraamancio1986@gmail.com" className="text-xl font-semibold hover:underline">Email</a>
+              <p className='text-gray-400 overflow-hidden md:break-normal break-all'>iaraamancio1986@gmail.com</p>         
             </div>
-            <a href="mailto:iaraamancio1986@gmail.com" className="text-xl font-semibold hover:underline">Email</a>
-            <p className='text-gray-400 overflow-hidden md:break-normal break-all'>iaraamancio1986@gmail.com</p>         
           </div>
-        </div>
-      </section>
+        </section>
+
+      </motion.div>
     </div>
   );
 }
