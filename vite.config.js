c1f427b7b5d7597bb0bb83e81d77c88e38a9f2fe
unknown <iaraamancio1986@gmail.com>
2025-12-11 12:35:51 -0300

    deploy via dist

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwincss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: `/Portfolio/`,
-  plugins: [react() , tailwincss()],
-})
